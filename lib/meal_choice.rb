63a112dbc06d01f56bb2521f05df0ad60dd098ce
Mason Balbera <mhbalbera@gmail.com>
2019-07-26 17:21:45 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,7 +1,9 @@
 # Your code here
 def meal_choice(protein = "meat",veg1,veg2)
 puts "What a nutritious meal!"
-if protein = ="broccoli", veg1 == "macaroni", veg2 =="tofu"
+if protein = ="broccoli" && veg1 == "macaroni" && veg2 =="tofu"
+  
+else
 puts "A plate of #{protein} with #{veg1} and #{veg2}." 
 "A plate of #{protein} with #{veg1} and #{veg2}."
 end

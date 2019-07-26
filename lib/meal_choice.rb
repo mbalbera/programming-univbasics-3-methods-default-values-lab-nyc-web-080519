9011be095c9554678bb28c2da87697dad5bb1abe
Mason Balbera <mhbalbera@gmail.com>
2019-07-26 17:07:45 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,15 +1,4 @@
 # Your code here
 
 puts "What a nutritious meal!"
-
-def meal_choice(*food)
-  str = "A plate of "
-  food.each_with_index do |f,i|
-    if str.length - 1 = i
-      str += "and #{f}."
-    else
-      str += "#{f}"
-    end
-  end
-    puts str
-end
+puts "A plate of #{protein} with #{veg1} and #{veg2}." 

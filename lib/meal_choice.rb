1a899818707c926090257aae5480d690582c9cd5
Mason Balbera <mhbalbera@gmail.com>
2019-07-26 16:59:57 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,11 +1,12 @@
 # Your code here
 def meal_choice(*food)
-    str = "A plate of "
-    food.each_with_index do |f,i|
+  str = "A plate of "
+  food.each_with_index do |f,i|
     if str.length - 1 = i
       str += "and #{f}."
     else
       str += "#{f}"
     end
+  end
     return str
 end
